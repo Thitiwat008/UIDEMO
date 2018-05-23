a870a2a93e036d612405e0d9161c9d9fcca2f68b
Thitiwat <thitiwat6338@gmail.com>
2018-05-23 12:26:00 +0700

    push Data To TableView

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,27 @@
 package sample;
 
+import adminDashBoard.dashBoardController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import javax.swing.*;
-
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     private final String id = "admin";
-    private final  String pass = "1234";
+    private final String pass = "1234";
 
     @FXML
     private TextField username;
@@ -27,24 +32,22 @@ public class Controller implements Initializable {
     @FXML
     private Label dbStatus;
 
-
     private loginModel loginModel = new loginModel();
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        if (this.loginModel.isDatabaseConnection()){
+        if (this.loginModel.isDatabaseConnection()) {
             this.dbStatus.setText("Connected to DB.");
-
         } else {
             this.dbStatus.setText("Not Connect to DB.");
         }
 
 
+
     }
 
     @FXML
-    public void Login(ActionEvent event) {
+    public void Login(ActionEvent event){
 //        try {
 //            if (id.equals(username.getText()) && pass.equals(password.getText())) {
 //                JOptionPane.showMessageDialog(null,
@@ -56,19 +59,17 @@ public class Controller implements Initializable {
 //                        JOptionPane.WARNING_MESSAGE);
 //            }
 //
-//        }catch (Exception e){
+//        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-//
-//
-//
-//
-//
-//    }
         try {
-            if (this.loginModel.isLogin(this.username.getText(),this.password.getText())){
-                JOptionPane.showMessageDialog(null,
-                        "Welcome to MIT SAIYAI APP");
+            if (this.loginModel.isLogin(this.username.getText(), this.password.getText())) {
+//                JOptionPane.showMessageDialog(null,
+//                        "Welcome to MIT SAIYAI APP");
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+                dashBoard();
+
             } else {
                 JOptionPane.showMessageDialog(null,
                         "Your username or password is invalid.",
@@ -78,13 +79,28 @@ public class Controller implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
 
 
+    }//Login
+
+    private void dashBoard() {
+        try {
+            Stage dashBoardStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = loader.load(getClass()
+                    .getResource("/adminDashBoard/dashBoard.fxml").openStream());
+            dashBoardController db = loader.getController();
+
+            Scene scene = new Scene(root);
+            dashBoardStage.setScene(scene);
+            dashBoardStage.setTitle("MIT DashBoard");
+            dashBoardStage.setResizable(false);
+            dashBoardStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-
-
-
+    }//dashBoard
 
 }//class
